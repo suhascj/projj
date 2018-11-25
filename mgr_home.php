2da533dea93a74c17b3fd6ade482a86ed200f5ff
suhascj <39197466+suhascj@users.noreply.github.com>
2018-11-25 18:02:08 +0530

    Update mgr_home.php

--- mgr_home.php
+++ mgr_home.php
@@ -12,7 +12,7 @@ background-size:100% 100%;
 
 </style>
 </head>
-<body background="images/4.jpg">
+<body background="4.jpg">
 
 <a href="index.php" style="float:right"><input type=button value=Logout style="height:30px;width:80px;border-radius:20px;background:#663300;opacity:0.7;border-color:#ffffff;color:white"></a><br><br><br>
 
